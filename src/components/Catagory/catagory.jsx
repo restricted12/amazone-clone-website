54c05b94dc817ad9861126071a24e379c20decd3
Ezedin <ezepayooner@gmail.com>
2025-02-10 00:28:25 -0800

    Update catagory2.jsx

--- src/components/Catagory/catagory.jsx
+++ src/components/Catagory/catagory.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { catagory } from '../../components/Catagory/AllInfo'; // Ensure this matches your export
-import CategoryCard from '../../components/Catagory/catagoryCard.jsx'; // Correct capitalization
+import CatagoryCard from '../../components/Catagory/catagoryCard.jsx'; // Correct capitalization
 import Classes from '../header/header.module.css';
+import '../bootstrap.css';
 
 function Category() {
   return (
-    <section className={Classes.category_container}>
-      {catagory.map((info) => (
-        <CategoryCard key={info.id} data={info} />
-      ))}
+    <section className={`${Classes.category_container} container py-4`}>
+      <div className="row justify-content-center">
+        {catagory.map((info) => (
+          <div key={info.id} className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+            <CatagoryCard data={info} />
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
