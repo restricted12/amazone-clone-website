1df26a5177b8dbc5c66d13292d430b8f311c40e1
Ezedin <ezepayooner@gmail.com>
2025-02-10 00:45:18 -0800

    Update catagoryCardrevv.jsx

--- src/components/Catagory/catagoryCard.jsx
+++ src/components/Catagory/catagoryCard.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import Classes from '../header/header.module.css';
 import { Link } from 'react-router-dom';
-// import './bootstrap.css';
+
 
 function CatagoryCard({ data }) {
   return (
-    <div className={`${Classes.catagory} card text-center p-3 shadow-sm h-100 d-flex flex-column align-items-center`}> 
-      <Link to={`/catagory/${data.category}`} className="text-decoration-none text-dark w-100"> 
+    <div className={Classes.catagory}>
+      <Link to={`/catagory/${data.category}`}> {/* You can specify the link dynamically */}
         <span>
-          <h2 className="h5 mt-2">{data.title}</h2>
+          <h2>{data.title}</h2>
         </span>
-        <img src={data.image} alt={data.title} className="img-fluid rounded w-100" style={{ maxHeight: '200px', objectFit: 'cover' }} />
-        <p className="btn btn-primary mt-2">Shop Now</p>
+        <img src={data.image} alt={data.title} />
+        <p>Shop Now</p>
       </Link>
     </div>
   );
