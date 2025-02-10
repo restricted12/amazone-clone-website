78404e3fe25d7afcc68b0c7d4bdf93da32380560
Ezedin <ezepayooner@gmail.com>
2025-02-09 23:04:32 -0800

    Update axios.js

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -3,5 +3,5 @@ import axios from 'axios';
 export const axiosInstance = axios.create({
   // baseURL: 'http://127.0.0.1:5001/clone-19120/us-central1/api',
   //  // Your backend base URL
-  baseURL:'https://amazon-api-deploy2-8dza.onrender.com/'
+  baseURL:'https://amazon-api-deploy2-q2jd.onrender.com/'
 });
