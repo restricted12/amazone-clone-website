73eb128403561c5696645adb6e2b2b07bd597aa9
Ezedin <ezepayooner@gmail.com>
2025-02-10 00:18:03 -0800

    Update courasoleeze.jsx

--- src/components/courasole/courasole.jsx
+++ src/components/courasole/courasole.jsx
@@ -1,29 +1,22 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
-import Classes from '../header/header.module.css';
+import Classes from '../header/header.module.css'
 import { img } from './img/courasol.js';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-// import './bootstrap.css';
-
 function CarouselComponent() {
   return (
-    <div className="container-fluid py-4">
-      <div className="row justify-content-center">
-        <div className="col-12 col-md-10 col-lg-8">
-          <Carousel
-            autoPlay={true}
-            infiniteLoop={true}
-            showIndicators={false}
-            showThumbs={false}
-            className="shadow rounded"
-          >
-            {img.map((imgItem, index) => (
-              <img key={index} src={imgItem} alt="Carousel Slide" className="img-fluid rounded" />
-            ))}
-          </Carousel>
-        </div>
-      </div>
-      <div className={`${Classes.hero_img} mt-4`}></div>
+    <div>
+      <Carousel
+        autoPlay={true}
+        infiniteLoop={true}
+        showIndicators={false}
+        showThumbs={false}
+      >
+        {img.map((imgItem) => {
+          return <img src={imgItem} alt="" />;
+        })}
+      </Carousel>
+      <div className={ Classes.hero_img}></div>
     </div>
   );
 }
